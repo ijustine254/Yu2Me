a0fa21570805bc0c6ec539913e520c6b112faadd
Justine <ontita254@gmail.com>
2022-12-12 13:07:24 +0300

    initial Commit

---
+++ Yu2Me/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Yu2Me project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Yu2Me.settings')
+
+application = get_asgi_application()
