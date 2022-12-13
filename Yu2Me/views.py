c89f1e32ecfa60937d6e9d8bdff8ae2b2d432250
Justine <ontita254@gmail.com>
2022-12-13 20:03:03 +0300

    added login page

--- Yu2Me/views.py
+++ Yu2Me/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, 'index.html')
-    
+    hasSession = request.session.get("logged")
+    if hasSession:
+        return render(request, 'index.html')
+    else:
+        # if has no session take to login page
+        return redirect(login)
+
+def login(request):
+    # Redirect on successful login
+    # return redirect(index)
+    if request.method == "POST":
+        pass
+    else:
+        return render(request, 'login.html')
+
+
+
