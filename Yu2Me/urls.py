94e98b6c27455afca75904ac63b2a4fa519b0220
Justine <ontita254@gmail.com>
2022-12-13 17:06:02 +0300

    cleaned url file

--- Yu2Me/urls.py
+++ Yu2Me/urls.py
@@ -17,7 +17,8 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index),
     path('admin/', admin.site.urls),
 ]
